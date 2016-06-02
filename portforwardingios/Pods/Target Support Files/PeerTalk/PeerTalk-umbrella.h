0235911b1632b9435f3e5e328d1382b8dde9f53f
Daniel Guillamot <dguillamot@gmail.com>
2016-06-02 15:36:26 +0900

    port forwarding ios client
    port forwarding ios client

---
+++ portforwardingios/Pods/Target Support Files/PeerTalk/PeerTalk-umbrella.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+#import "PTChannel.h"
+#import "PTProtocol.h"
+#import "PTUSBHub.h"
+
+FOUNDATION_EXPORT double PeerTalkVersionNumber;
+FOUNDATION_EXPORT const unsigned char PeerTalkVersionString[];
+
